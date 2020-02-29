09746b40e2d65aeb426be4dc0db3941ffd27d716
akio <akiooarao@gmail.com>
2020-02-29 12:06:42 -0600

    Practica 3

---
+++ practica3/actores.js
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------
+ * Práctica 3: Utilizando promesas y XML
+ * Fecha: 05-Mar-2020
+ * Autora: A01651395 Jorge Akio Olvera Arao
+ *----------------------------------------------------------*/
+
+const fs = require('fs');
+const util = require('util');
+const xml2js = require('xml2js');
+
+const parser = new xml2js.Parser();
+const createReadFilePromise = util.promisify(fs.readFile);
+const createParseXmlPromise = util.promisify(parser.parseString);
+
+/**
+ * Reads an XML file and parses it into JSON data.
+ * @param {string} path The path of the XML file to read from
+ * @return {Promise<Object>} Resolves into the JSON data converted from XML
+ */
+async function getJsonFromXmlFile(path) {
+  let xmlData = await createReadFilePromise(path);
+  return await createParseXmlPromise(xmlData);
+}
+
+/**
+ * Formats a 'movie' object into a string with the movie name,
+ * followed by it's cast with each actor in a new line and indented
+ * with two whitespaces. If the movie has no cast registered, the 
+ * string will only be the name of the movie.
+ * The string will always have a blank new line at the end.
+ * @param {Object} movie Object that represents a movie
+ * @return {string} The formatted string for the movie's cast
+ */
+function formatMovieCast(movie) {
+  let name = movie['$']['name'];
+  let formattedCast = `${name}\n`;
+  if ('cast' in movie) {
+    let cast = movie['cast'];
+    cast.forEach(actor => {
+      actor = actor.trim();
+      formattedCast += `  ${actor}\n`
+    })
+  }
+  return formattedCast;
+}
+
+/**
+ * Reads movies from an XML file and prints the movie name and it's
+ * cast through standard output.
+ * The movie cast is represented it's actors names, each in a new line and 
+ * indented with two whitespaces. If the movie has no cast registered, only the 
+ * movie name is shown.
+ * Each movie cast is separated by a new line.
+ * @return {Promise<undefined>}
+ */
+async function printCastWithFormat() {
+  try {
+    let movies = (
+      await getJsonFromXmlFile(`${__dirname}/movies.xml`)
+    )['movies']['film'];
+    let formattedMoviesCast = movies.map(movie => formatMovieCast(movie))
+    console.log(formattedMoviesCast.join('\n'));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+printCastWithFormat();
---
+++ practica3/peliculas.js
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------
+ * Práctica 3: Utilizando promesas y XML
+ * Fecha: 05-Mar-2020
+ * Autora: A01651395 Jorge Akio Olvera Arao
+ *----------------------------------------------------------*/
+
+const fs = require('fs');
+const util = require('util');
+const xml2js = require('xml2js');
+
+const parser = new xml2js.Parser();
+const createReadFilePromise = util.promisify(fs.readFile);
+const createParseXmlPromise = util.promisify(parser.parseString);
+
+/**
+ * Reads an XML file and parses it into JSON data.
+ * @param {string} path The path of the XML file to read from
+ * @return {Promise<Object>} Resolves into the JSON data converted from XML
+ */
+async function getJsonFromXmlFile(path) {
+  let xmlData = await createReadFilePromise(path);
+  return await createParseXmlPromise(xmlData);
+}
+
+/**
+ * Formats a 'movie' object into a string with its name, followed by
+ * the year of release and it's director if it exsists. For example:
+ * 'Example movie (1990), director: John Smith'
+ * or 'Example movie (1990)' if the director isn't registered
+ * @param {Object} movie Object that represents a movie
+ * @return {string} The formatted string for a movie
+ */
+function formatMovie(movie) {
+  let name = movie['$']['name'];
+  let year = movie['$']['year'];
+  let movieDescription = `${name} (${year})`;
+  if ('director' in movie) {
+    let director = movie['director'][0].trim();
+    movieDescription += `, director: ${director}`;
+  }
+  return movieDescription;
+}
+
+/**
+ * Reads movies from an XML file and prints them through standard output
+ * in following format: Example movie (1990), director: John Smith.
+ * Every movie will be printed in a new line and if the movie doesn't
+ * have a director registered, that part of the string won't be shown.
+ * @return {Promise<undefined>}
+ */
+async function printMoviesWithFormat() {
+  try {
+    let movies = (
+      await getJsonFromXmlFile(`${__dirname}/movies.xml`)
+    )['movies']['film'];
+    let formattedMovies = movies.map(movie => formatMovie(movie));
+    console.log(formattedMovies.join('\n'));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+printMoviesWithFormat();
