46b373dfca4ac92c8366a9b7e6109d45b710f503
akio <akiooarao@gmail.com>
2020-03-15 21:27:23 -0600

    Finished practica 5

--- practica5/routes/index.js
+++ practica5/routes/index.js
@@ -10,16 +10,36 @@ let router = express.Router();
 let sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('super.db');
 
-/* GET home page. */
-router.get('/', async function (req, res, next) {
-  let sql = 'select * from superheroes;';
-
-  db.all(sql, [], (err, rows) => {
-    if (err) throw err;
-    rows.forEach((row) => {
-      res.render('index', { rows });
+/**
+ * Makes a query to read the database and returns that information.
+ * @param {string} sql The query to be made
+ * @return {Promise<Object[]>} Resolves into an array of the row objects
+ */
+function makeQuery(sql) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
   });
+}
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  let sql = `
+    select * 
+    from superheroes
+    order by "Sexo" asc, "Identidad secreta" asc;
+  `;
+  try {
+    let rows = await makeQuery(sql);
+    res.render('index', { rows });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
